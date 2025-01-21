86f51c3a7918a054c229a27fc213c350896b2b93
PikachuGV <gavyn.hor.jr@gmail.com>
2025-01-21 22:26:53 +0800

    Q16

--- sol.c
+++ sol.c
@@ -1,5 +1,6 @@
 /*
-Paste question here.
+Q16: 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26
+What is the sum of the digits of the number 2^1000?
 */
 
 #include <stdlib.h>
@@ -8,5 +9,26 @@ Paste question here.
 #include <string.h>
 
 int main() {
+    short* digits = malloc(sizeof(short)); //First digit at the end, last at the front
+    digits[0] = (short)1;
+    int newdigit, carryover = 0, ndigits = 1, sum = 0;
+    
+    for (int i = 0; i < 1000; i++) {
+        if (digits[ndigits - 1] >= 5) {
+            ndigits++;
+            digits = realloc(digits, ndigits * sizeof(short));
+            digits[ndigits - 1] = 0;
+        }
+        for (int j = 0; j < ndigits; j++) {
+            newdigit = digits[j] * 2 + carryover;
+            carryover = newdigit / 10;
+            digits[j] = newdigit % 10;
+        }
+    }
+
+    for (int i = 0; i < ndigits; i++) sum += digits[i];
+
+    printf("%d\n", sum);
+
     return 0;
 }
