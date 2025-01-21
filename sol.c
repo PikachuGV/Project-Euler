f6e439eb5948c7461c910d4f462944c4d87a7750
PikachuGV <gavyn.hor.jr@gmail.com>
2025-01-21 22:20:23 +0800

    Q6

--- sol.c
+++ sol.c
@@ -1,5 +1,5 @@
 /*
-The sum of the squares of the first ten natural numbers is,
+Q6: The sum of the squares of the first ten natural numbers is,
 1^2 + 2^2 + ... + 10^2 = 385.
 The square of the sum of the first ten natural numbers is,
 (1 + 2 + ... + 10)^2 = 55^2 = 3025.
