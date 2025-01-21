b012ca0dc4992e568da2dcfb2b82da946dddf271
PikachuGV <gavyn.hor.jr@gmail.com>
2025-01-21 22:18:04 +0800

    Q9

--- sol.c
+++ sol.c
@@ -1,5 +1,8 @@
 /*
-Paste question here.
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a^2 + b^2 = c^2.
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2. 
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
 */
 
 #include <stdlib.h>
@@ -8,5 +11,26 @@ Paste question here.
 #include <string.h>
 
 int main() {
+    int squares[1001];
+    for (int i = 0; i < 1001; i++) {
+        squares[i] = i * i;
+    }
+
+    int f = 0;
+    int a, b, c;
+    bool done = false;
+
+    while (f < 1001 && !done) {
+        a = squares[f];
+        for (int i = f + 1; i < 1001; i++) {
+            if (1000 - i - f >= 1001) continue;
+            b = squares[i]; c = a + b;
+            if (c == squares[1000 - f - i]) {
+                printf("%d\n", f * i * (1000 - f - i));    
+            }
+        }
+        f++;
+    }
+
     return 0;
 }
