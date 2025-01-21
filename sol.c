9512673565417b30c42c10760ce0e95b1126fc5b
PikachuGV <gavyn.hor.jr@gmail.com>
2025-01-21 22:03:58 +0800

    A

--- sol.c
+++ sol.c
@@ -1,5 +1,6 @@
 /*
-Paste question here.
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143?
 */
 
 #include <stdlib.h>
@@ -8,5 +9,20 @@ Paste question here.
 #include <string.h>
 
 int main() {
+    long long n = 600851475143;
+    while (n % 2 == 0) {
+        n >>= 1;
+    }
+
+    for (int i = 3; i * i < n; i += 2) {
+        if (n % i == 0) {
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+    }
+
+    int maxPrime = n == 1 ? 2 : n;
+    printf("%d\n", maxPrime);
     return 0;
 }
