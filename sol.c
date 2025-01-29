ab4520bdba636f959696cbb3f42408163e08aaf4
PikachuGV <gavyn.hor.jr@gmail.com>
2025-01-29 19:56:39 +0800

    Q39

--- sol.c
+++ sol.c
@@ -1,5 +1,18 @@
 /*
-Paste question here.
+If p is the perimeter of a right angle triangle with integral length sides, {a, b, c}, there are exactly three solutions for p = 120.
+{20,48,52}, {24,45,51}, {30,40,50}
+For which value of p <= 1000, is the number of solutions maximised?
+*/
+
+/*
+This question is mainly asking us to find Pythogorean Triplets and then finding the sum of these triplets.
+
+Euclid formula can help us find primitive Pythogarean Triplets quickly.
+
+If we have integers m, n such that m and n are coprime, then:
+a = m^2 - n^2, b = 2mn, c = m^2 + n^2 will form a (a,b,c) primitive triplet.
+
+
 */
 
 #include <stdlib.h>
