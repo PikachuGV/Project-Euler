63989121929fcf3d2e90adcb9f763dc6cf09c82a
PikachuGV <gavyn.hor.jr@gmail.com>
2025-02-01 14:46:44 +0800

    Q43

--- sol.c
+++ sol.c
@@ -1,5 +1,14 @@
 /*
-Paste question here.
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+d2d3d4=406 is divisible by 2
+d3d4d5=063 is divisible by 3
+d4d5d6=635 is divisible by 5
+d5d6d7=357 is divisible by 7
+d6d7d8=572 is divisible by 11
+d7d8d9=728 is divisible by 13
+d8d9d10=289 is divisible by 17
+Find the sum of all 0 to 9 pandigital numbers with this property.
 */
 
 #include <stdlib.h>
@@ -7,6 +16,29 @@ Paste question here.
 #include <stdbool.h>
 #include <string.h>
 
+void swap (int *i, int *j) {
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+void permute(int index, int* arr, int n) {
+   if (index == n-1) {
+       for (int k = 1; k < n; ++k) {
+            
+       }
+       return;
+   }
+   for (int i = index; i < n; i++) {
+       swap (arr + index, arr + i);
+       permute (index+1, arr, n);
+       swap (arr + i, arr + index);
+   }
+   return;
+}
+
 int main() {
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    permute(0, arr, 10);
     return 0;
 }
