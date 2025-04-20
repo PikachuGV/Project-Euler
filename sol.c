c0d766e4774096b06363470084fc102fe5962325
PikachuGV <gavyn.hor.jr@gmail.com>
2025-04-20 21:11:21 +0800

    Q65

--- sol.c
+++ sol.c
@@ -31,21 +31,56 @@ We can first generate all ai for 0<=i<100. Then use the recurrence to help us ge
 #include <stdbool.h>
 #include <string.h>
 
-#define ULL unsigned long long
+char *add(char *a, char *b) {
+    if (strlen(a) > strlen(b)) {
+        char *temp = malloc(strlen(b) * sizeof(char)); strcpy(temp, b);
+        char *zeros = calloc(strlen(a) - strlen(b) + 1, sizeof(char));
+        memset(zeros, '0', (strlen(a) - strlen(b)) * sizeof(char));
+        b = calloc(strlen(a) + 1, sizeof(char));
+        strcat(b, zeros); strcat(b, temp);
+        free(zeros); free(temp);
+    }
+    int len = strlen(a);
+    int digit = 0, carry = 0;
+    
+    char *c = calloc((len + 2), sizeof(char));
+    
+    for (int i = len - 1; i >= 0; i--) {
+        digit = ((a)[i] - '0') + ((b)[i] - '0') + carry;
+        carry = digit / 10;
+        /* if (carry > 9 || carry < 0) {
+            ("%s, %s, %d", (a), (b), carry);
+            exit(100);
+        } */
+        c[i + 1] = (digit % 10) + '0';
+    }
 
-ULL* gena(int N) {
-    ULL* arr = (ULL*)malloc(N * sizeof(ULL));
-    for (int i = 1; i < N; i++) arr[i] = 1;
-    arr[0] = 2;
+    if (carry == 0) {
+        char *C = calloc((len + 1), sizeof(char));
+        memcpy(C, c + 1, len * sizeof(char));
+        free(c);
+        return C;
+    }
+    c[0] = carry + '0';
+    return c;
+}
 
-    for (ULL i = 2; i < N; i += 3) {
-        arr[i] = 2 * (i + 1) / 3;
+char** gena(int N) {
+    char** arr = (char**)malloc(N * sizeof(char*));
+    for (int i = 1; i < N; i++) arr[i] = "1";
+    arr[0] = "2";
+
+    char *multiple = "0";
+
+    for (int i = 2; i < N; i += 3) {
+        multiple = add(multiple, "2");
+        arr[i] = multiple;
     }
 
     return arr;
 }
 
-int sumdigits(ULL n) {
+int sumdigits(char n) {
     int sum = 0;
     while (n != 0) {
         sum += (n % 10);
@@ -57,12 +92,12 @@ int sumdigits(ULL n) {
 
 int main() {
     const int N = 100;
-    ULL* a = gena(N);
+    char** a = gena(N);
 
-    ULL pa = 1LL, pb = (long long)a[0], pc;
+    char *pa = "1", *pb = a[0], *pc;
 
     for (int i = 1; i < N; i++) {
-        pc = a[i] * pb + pa;
+        pc = add(a[i] * pb + pa;
         pa = pb; pb = pc;
         printf("%llu ", pc);
     }
