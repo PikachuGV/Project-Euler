644082a3b48caca1cce5a8d04e793fdc93556575
PikachuGV <gavyn.hor.jr@gmail.com>
2025-07-02 21:30:27 +0800

    Q78

--- sol.c
+++ sol.c
@@ -27,8 +27,9 @@ It is taken that p(0) = 1 and p(k) = 0 for negative k.
 #include <stdbool.h>
 #include <string.h>
 
-#define N 100
+#define N 100000
 typedef unsigned long long ULL;
+
 
 void genPen(ULL** arr, int n) {
     *arr = malloc(n * sizeof(ULL));
@@ -41,24 +42,32 @@ void genPen(ULL** arr, int n) {
 
 
 int main() {
-    ULL *p = calloc(N, sizeof(ULL));
+    int *p = calloc(N, sizeof(int));
     ULL *pen; genPen(&pen, N);
 
     p[0] = 1;
 
-    ULL P; int index;
+    int P; int index;
 
     for (int i = 1; i < N; i++) {
         P = 0; index = 0;
         for (int j = 0; j < N; j++) {
             if (i < pen[j]) break;
             index = i - pen[j];
-            P = ((j % 4) < 2) ? P + p[index] : P - p[index];
+            if ((j % 4) < 2) {
+                P += p[index];
+            } else {
+                P -= p[index];
+            }
         }
+
+        P %= 1000000;
+        if (P < 0) P += 1000000;
         p[i] = P;
+
+        if (P == 0) {
+            printf("%d ",i); break;
+        }
     }
-
-
-    for (int i = 0; i < N; i++) printf("%llu ", p[i]);
 }
 
